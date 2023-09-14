e9fe40a770765b00781b299726a21e3135dd5a1b
BIGAD GABER <BIGADGABER@icloud.com>
2023-09-14 15:04:41 -0500

    Assign name to each clicked square

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const gameboard = ((availableSquares) => {
     else {
         return gameBoardArray = ['1A', '1B', '1C', '2A', '2B', '2C', '3A', '3B', '3C'] //numb = row, letter = column
     }
+
+    //if the process of feeding gameBoardArray back and forth between gameboard and flowOfGame, try returning a diff
+    //name from gameboard, and recieving that same name into flowOfGame
 })(availableSquares);
 
 const player = (userName, squareSelection) => {
@@ -15,13 +18,28 @@ const player = (userName, squareSelection) => {
 };
 
 const squares = document.querySelector('.squares');
+const gameBoardDiv = document.querySelector('.game-board')
 
-const flowOfGame = ((gameBoardArray) => {
-    squares.addEventListener('click', event => {
-        let btn = event.target.closest("button");
+
+//const flowOfGame = ((gameBoardArray) => {
+
+        //assign a square title to each square (ex. 1A, 1B, etc.)
+    gameBoardDiv.addEventListener('click', event => {
+        let squareSelection = event.target.closest("button");
+
+        if (!squareSelection || !gameBoardDiv.contains(squareSelection)) return;
+
+        let buttonID = squareSelection.id;
+        console.log(buttonID);
         
-        if (!btn || !squares.contains(btn)) return;
+
     });
-    
-    return {availableSquares, squareSelection, computerSelection}
-})();
+
+
+
+        //add func to toggle an 'X' on to the button that's clicked
+        //add func to randomly select a square for computerSelection
+        //run the func to change the gameBoardArray value again after this
+
+ //   return {availableSquares, squareSelection, computerSelection}
+//})(gameBoardArray);
