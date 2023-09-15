95fb507183a4e2d8e7c177b9116c56eb8b5e4841
BIGAD GABER <BIGADGABER@icloud.com>
2023-09-15 12:30:19 -0500

    assign X and O locations to their own arrays

--- script.js
+++ script.js
@@ -11,6 +11,10 @@ const Gameboard = (() => {
         return arr[randomIndex];
     }
 
+    let xArray = [];
+    let oArray = [];
+
+
     function handleSquareClick(event) {
         let squareSelection = event.target.closest("button");
 
@@ -18,9 +22,11 @@ const Gameboard = (() => {
 
         let buttonID = squareSelection.id;
 
+        //add selected squares to xArray and remove them from gameBoardArray
         let index = gameBoardArray.indexOf(buttonID);
         if (index !== -1) {
-            gameBoardArray.splice(index, 1);
+            let splicedItems = gameBoardArray.splice(index, 1);
+            xArray.push(...splicedItems);
         } 
 
         squareSelection.innerText = 'X';
@@ -34,8 +40,14 @@ const Gameboard = (() => {
 
         let indexComp = gameBoardArray.indexOf(computerSelection);
         if (indexComp !== -1) {
-            gameBoardArray.splice(indexComp, 1);
-        } 
+            let splicedComp = gameBoardArray.splice(indexComp, 1);
+            oArray.push(...splicedComp);        } 
+
+
+        console.log(`xArray: ${xArray}`)
+        console.log(`oArray: ${oArray}`)
+        console.log(`gameBoardArray: ${gameBoardArray}`)
+
 
         return { availableSquares: gameBoardArray, squareSelection, computerSelection };
     }
@@ -48,14 +60,13 @@ gameBoardDiv.addEventListener('click', Gameboard.handleSquareClick);
 
 
 
-
-//figure out proper connection between eventlistenter and const gameboard. rn gameboard seems mostly redundant
-
-//make game end once someboday gets 3 in a row, display message with this (figure out how reseting the game is going to
-//work so that it's not only a refresh, or maybe it should be?)
+//make game end once someboday gets 3 in a row. Display message with this. Steps:
+// --find pattern in array--Maybe: if 3 array items contain same letter or number, game ends. For diagonal: if the specific
+//pattern of [1A, 2B, 3C] or [1C, 2B. 3A] exists, game ends
+// -- Then worry about popup and game reset process (through page reload or other method)
 
 //make game end once all squares are filled, display message with this
 
 //make a button to start/restart the game
 
-//Clean up the interface to allow players to put in their names, 
+//Clean up the interface to allow players to put in their names, use 'player' fact func for this
