6cbc8047838bba0478fd45aeb4c0ae7c228d8029
BIGAD GABER <BIGADGABER@icloud.com>
2023-09-17 08:51:36 -0500

    make function to detect 3 in a row
    Works for all 3 in a row placements except diagonally

--- script.js
+++ script.js
@@ -37,7 +37,8 @@ const Gameboard = (() => {
         setTimeout(function() {
             computerSquare.innerText = 'O';
         }, 1000);
-
+        
+        //add computer squares to oArray and remove them from gameBoardArray
         let indexComp = gameBoardArray.indexOf(computerSelection);
         if (indexComp !== -1) {
             let splicedComp = gameBoardArray.splice(indexComp, 1);
@@ -48,9 +49,43 @@ const Gameboard = (() => {
         console.log(`oArray: ${oArray}`)
         console.log(`gameBoardArray: ${gameBoardArray}`)
 
+        checkForRepeatingCharacters(xArray);
+        checkForRepeatingCharacters(oArray);
+
 
         return { availableSquares: gameBoardArray, squareSelection, computerSelection };
     }
+
+
+
+
+
+//replace this example func with the func for ending the game/announcing winner
+function exampleFunction(char) {
+    console.log(`The character "${char}" appears three or more times.`);
+}
+
+function checkForRepeatingCharacters(items) {
+    // Create a Map to store character counts
+    let charCount = new Map();
+
+    // Flatten the array into a single string
+    let flattened = items.join('');
+
+    for (let char of flattened) {
+        charCount.set(char, (charCount.get(char) || 0) + 1);
+    }
+
+    for (let [char, count] of charCount.entries()) {
+        if (count >= 3) {
+            exampleFunction(char);
+            return;
+        }
+    }
+}
+
+
+
 
     return { player, handleSquareClick };
 })();
@@ -60,9 +95,9 @@ gameBoardDiv.addEventListener('click', Gameboard.handleSquareClick);
 
 
 
-//make game end once someboday gets 3 in a row. Display message with this. Steps:
+//make game end once someboday gets 3 in a row. Display message with winner. Steps:
 // --find pattern in array--Maybe: if 3 array items contain same letter or number, game ends. For diagonal: if the specific
-//pattern of [1A, 2B, 3C] or [1C, 2B. 3A] exists, game ends
+//pattern of [1A, 2B, 3C] or [1C, 2B. 3A] exist, game ends
 // -- Then worry about popup and game reset process (through page reload or other method)
 
 //make game end once all squares are filled, display message with this
